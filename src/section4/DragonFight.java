3454f33caa2591ab7f12dfc39ef732848a5c6a84
League Student <league@iMac-18.attlocal.net>
2019-08-01 14:26:34 -0700

    day 4

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -5,62 +5,96 @@ import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 
 public class DragonFight {
-	// 1. Put all of the game code inside the main method (below) 
-	
-	public static void main(String[] args) {
-		
-		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");		
-		
-		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
-		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
-		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
-		// 4. Create a variable to hold the damage the player's attack does each round
-		
-		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
-		
-		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
-		
-		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
-		
-		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
-		// 9. If they typed in "yell":
-		
-			//-- Find a random number between 0 and 10 and store it in dragonDamage
-			
-			//-- Subtract that number from the dragon's health variable 
-			
-		// 10. If they typed in "kick":
-		
-			//-- Find a random number between 0 and 25 and store it in dragonDamage
-			
-			//-- Subtract that number from the dragon's health variable
-			
-			
-		
-		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
-		// 12. Subtract this number from the player's health
-		
-		
-		
-		// 13. If the user's health is less than or equal to 0
-		
-			//-- Tell the user that they lost
-			
-		
-		// 14. Else if the dragon's health is less than or equal to 0
-		
-			//-- Tell the user that the dragon is dead and they took a ton of gold!
-			
-	   //  15.  Else
-			
-			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
-			
-		}
-	}
+	// 1. Put all of the game code inside the main method (below)
 
+	public static void main(String[] args) {
+
+		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");
+
+		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
+
+	
+	// 2. Create a variable called "playerHealth" to store your health (set it equal
+	// to 100)
+
+	int playerhealth = 100;
+
+	// 3. Create a variable called "dragonHealth" to store the dragon's health (set
+	// it equal to 100)
+
+	int dragonhealth = 100;
+
+	// 4. Create a variable to hold the damage the player's attack does each round
+
+	int playerdamage = 10;
+
+	// 5. Create a variable to hold the damage the dragon's attack does each round
+
+	int dragondamage = 10;
+
+	// 6. Delete the slashes at the beginning of the next line.
+	while(playerhealth>0&&dragonhealth>0)
+	{ // this line of code keeps the battle going until someone's health reaches 0
+
+		// 7. Add a closing mustache at the very bottom of this program (since we just
+		// added an opening mustache on the previous step).
+
+		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell
+		// or a kick
+		String attack = JOptionPane.showInputDialog(null, "Do you want to attack the dragon with a yell or a kick?");
+
+		// 9. If they typed in "yell":
+
+		if (attack.equals("yell")) {
+
+			dragondamage = new Random().nextInt(10);
+			dragonhealth = dragonhealth - dragondamage;
+		}
+		// -- Find a random number between 0 and 10 and store it in dragonDamage
+
+		// -- Subtract that number from the dragon's health variable
+
+		// 10. If they typed in "kick":
+
+		if (attack.equals("kick")) {
+
+			dragondamage = new Random().nextInt(25);
+			dragonhealth = dragonhealth - dragondamage;
+		}
+		// -- Find a random number between 0 and 25 and store it in dragonDamage
+
+		// -- Subtract that number from the dragon's health variable
+
+		// 11. Find a random number between 0 and 35 and store it in playerDamage
+		playerdamage = new Random().nextInt(35);
+		playerhealth = playerhealth - playerdamage;
+
+		// 12. Subtract this number from the player's health
+
+		// 13. If the user's health is less than or equal to 0
+
+		// -- Tell the user that they lost
+		if (playerhealth <= (0)) {
+			JOptionPane.showMessageDialog(null, "You lost.");
+		} else if (dragonhealth <= (0)) {
+
+			JOptionPane.showMessageDialog(null, "Good job, the dragon is dead and you took a ton of gold!");
+
+		} else {
+
+			JOptionPane.showMessageDialog(null, "your health = 1, dragon helath = 100");
+		}
+
+	}
+}}
+
+
+// 14. Else if the dragon's health is less than or equal to 0
+
+// -- Tell the user that the dragon is dead and they took a ton of gold!
+
+// 15. Else
+
+// -- Pop up a message that tells the their current health and the dragon's
+// currently health (Bonus: Also display the amount of health that was lost for
+// each player this round)
--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -15,46 +15,65 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
-	// 1. Create a new mini robot (type "mini" inside the parentheses)
+// 1. Create a new mini robot (type "mini" inside the parentheses)
+	
+	Robot Steve = new Robot ("mini");
 	
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
+		Steve.setAngle(0);
+		Steve.microMove(10);
 		
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+		Steve.setAngle(180);
+		Steve.microMove(10);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+Steve.setAngle(90);
+Steve.microMove(10);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+		Steve.setAngle(90);
+		Steve.microMove(10);
 	}
 
 	private void spaceBarWasPressed() {
 
 		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		int robotXLocation = Steve.getX();
+		int robotYLocation = Steve.getY();
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+		System.out.println(robotXLocation + "     "+ robotYLocation);
 		// 7. If robot is at same location as the little girl
-		//      --make a pop-up tell the robot where to go next
+		//--make a pop-up tell the robot where to go next
+		
+		
+		if (robotXLocation== 730 &&robotYLocation==440) {
+			JOptionPane.showMessageDialog(null, "Go home and pet your cat.");
+		}
+		
 		
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
 		
+		if (robotXLocation== 470 && robotYLocation==550) {
+			JOptionPane.showMessageDialog(null, "OH DEAR ITS A DEAD FISH GET OUT THE WAY");
+		}
+		
 		// 9.  If the robot is in the final location
 		//     --call the treasureFound() method
-		
-	}
+		if (robotXLocation==470 && robotYLocation==550) {
+		JOptionPane.showMessageDialog(null, "jeez the dead fish was the treasure, life leassons right here. the treasure is where you least expect it. haha. still go home and pet your cat.");
+
+		}}
 
 	private void go() {
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
